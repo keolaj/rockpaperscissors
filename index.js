85dc19710a887a36d4715849ed78568534f87538
keolaj <keolajarvegren@gmail.com>
2022-07-28 16:48:44 -0700

    added html and functionality

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ function getComputerChoice() {
 	}
 }
 
+function runGame(userChoice, computerChoice) {
+	if (userChoice === "Rock" && computerChoice === "Scissors") {
+		return "You Win!!"
+	}
+	if (userChoice === "Paper" && computerChoice === "Rock") {
+		return "You Win!!"
+	}
+	if (userChoice === "Scissors" && computerChoice === "Paper") {
+		return "You Win!!"
+	}
+	if (userChoice === "Rock" && computerChoice === "Paper") {
+		return "You Lose!!"
+	}
+	if (userChoice === "Paper" && computerChoice === "Scissors") {
+		return "You Lose!!"
+	}
+	if (userChoice === "Scissors" && computerChoice === "Rock") {
+		return "You Lose!!"
+	}
+	if (userChoice === computerChoice) {
+		return "You Tie!!"
+	}
+}
+
+document.querySelector(".button-section").childNodes.forEach((node) => {
+	node.addEventListener("click", () => {
+		let winText = runGame(node.textContent, getComputerChoice());
+		let textSection = document.querySelector(".text-section");
+		let text = textSection.textContent;
+		textSection.textContent = text + '\n' + winText;
+
+	})
+})
